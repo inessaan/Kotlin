6f831e603d8cedfd519cd4426932703478ebc800
Inessa An <inessa.an004@mail.ru>
2021-04-17 20:00:36 +0300

    Задание 5

--- lab 13/src/helloworld.kt
+++ lab 13/src/helloworld.kt
@@ -1,5 +1,13 @@
+fun whichLanguage (a:String, b:String) =
+    when{
+        a=="Prolog" -> println("$b,ну Вы и подлиза")
+        a=="Kotlin" -> println("$b, ну Вы и подлиза")
+        else -> println("$b, мне он не нравится >:| ")
+    }
 fun main() {
-    println("Как вас зовут?")
-    val  name = readLine()
-    println("Здравствуйте, $name!")
+    println("Как Вас зовут?")
+    val name = readLine()
+    println("Здравствуйте, $name! Какой Ваш любимый язык программирования?")
+    val lang = readLine()
+    whichLanguage(lang.toString(), name.toString())
 }
