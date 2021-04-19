233b9985cbae16c50418b2c9bd8be78bb67fb1e6
Inessa An <inessa.an004@mail.ru>
2021-04-19 21:07:13 +0300

    Задание 7

--- lab 13/src/helloworld.kt
+++ lab 13/src/helloworld.kt
@@ -1,15 +1,36 @@
-fun sumDig(x: Int): Int {
-    var s = 0
+fun min(x: Int): Int{
+    var min = 9
     var temp: Int = x
     while (temp != 0) {
-        s += temp % 10
+        if (min > temp % 10)
+            min = temp % 10
         temp /= 10
     }
-    return (s)
+    return (min)
+}
+
+fun max(x: Int): Int{
+    var max = 0
+    var temp: Int = x
+    while (temp != 0) {
+        if (max < temp % 10)
+            max = temp % 10
+        temp /= 10
+    }
+    return (max)
+}
+
+fun multiply(x: Int): Int{
+    var p = 1
+    var temp: Int = x
+    while (temp != 0) {
+        p*= temp % 10
+        temp /= 10
+    }
+    return (p)
 }
 fun main() {
     println("Введите число: ")
     val x = readLine()!!.toInt()
-    print("Сумма цифр числа = ")
-    println(sumDig(x))
+    print("Максимальная цифра числа = ${max(x)} \nМинимальная цифра числа = ${min(x)} \n Произведение цифр числа = ${multiply(x)}")
 }
