351ee9bde85e8bd7b5c933e3ae01a7307ab52e2d
Inessa An <inessa.an004@mail.ru>
2021-04-24 01:18:21 +0300

    Задание 8.3

--- lab 13/src/helloworld.kt
+++ lab 13/src/helloworld.kt
@@ -1,16 +1,26 @@
-fun odd(x:Int):Int{
-    var sum=0
+fun sumDig(x: Int): Int {
+    var sum = 0
     var temp: Int = x
     while (temp != 0) {
-        if (temp % 10 > 3 && (temp % 10) % 2 !=0)
-            sum += 1
+        sum += temp % 10
         temp /= 10
     }
     return sum
 }
-
+fun multDiv(x:Int):Int{
+    var mult = 1
+    var sumX = sumDig(x)
+    for(i in 2..x) {
+        if (x % i == 0) {
+            val sumI = sumDig(i)
+            if(sumI<sumX)
+                mult *= sumI
+        }
+    }
+    return mult
+}
 fun main() {
     println("Введите число: ")
     val x = readLine()!!.toInt()
-    print("Количество нечетных цифр числа, больших 3 = ${odd(x)}")
+    print("Произведение делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа = ${multDiv(x)}")
 }
