d55c355d68ab6c290a56ba80afd40e0bebb8162f
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-05-11 02:56:00 +0300

    Задание 1 лаб14

---
+++ lab14/src/lab14.kt
@@ -0,0 +1,6 @@
+fun sumUp(n: Int): Int = if (n == 0) n else sumUp(n/10)+n%10
+fun main() {
+    println("Введите число: ")
+    val x = readLine()!!.toInt()
+    print("Сумма цифр вверх: ${sumUp(x)}\n")
+}
