f68484b9ce434cdc18ee61b4b6868f807b4e4ea5
Inessa An <72630506+inessaan@users.noreply.github.com>
2021-05-11 03:16:53 +0300

    ошибка с локальным репозиторием, перезалив 14 лабы

--- lab14/src/lab14.kt
+++
@@ -1,6 +0,0 @@
-fun sumUp(n: Int): Int = if (n == 0) n else sumUp(n/10)+n%10
-fun main() {
-    println("Введите число: ")
-    val x = readLine()!!.toInt()
-    print("Сумма цифр вверх: ${sumUp(x)}\n")
-}
