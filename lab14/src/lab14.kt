b2789a4c5d1b86c62ba8c3ef1bac7e2263e4fdc1
Inessa An <inessa.an004@mail.ru>
2021-05-11 05:47:07 +0300

    Задание 5 лаб14

--- lab14/src/lab14.kt
+++ lab14/src/lab14.kt
@@ -1,19 +1,18 @@
-
-tailrec fun down(n: Int, acum : Int, f : (Int, Int) -> Int) : Int =
+tailrec fun downCond(n: Int, acum : Int, f : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
     if ( n == 0) acum else
-        down(n / 10, f(n % 10, acum), f)
+        downCond(n/10, if (pr(n % 10)) f (n % 10, acum) else acum, f, pr)
 
-fun sum(n: Int) : Int = down(n, 0, {a,b -> a + b})
-fun mult(n: Int) : Int = down(n, 1, {a,b -> a * b})
-fun max(n: Int) : Int = down(n, 0, {a,b -> if (a > b) a else b})
-fun min(n: Int) : Int = down(n, 9, {a,b -> if (a < b) a else b})
 
+fun sumCond(n: Int) : Int = downCond(n, 0, {a,b -> a + b}, {x -> x % 2 != 0 && x>3}) // сумма нечетных цифр, больших 3, нач счетчик равен 0
+fun multCond(n: Int) : Int = downCond(n, 1, {a,b -> a * b}, {x -> x % 2 != 0 && x<6}) // произведение нечетных цифр, меньших 6, нач счетчик равен 1
+fun maxCond(n: Int) : Int = downCond(n, 0, {a,b -> if (a > b) a else b}, {x -> x % 2 == 0 && x<8}) // максимальная четная цифру, меньше 8, нач знач равно 0
+fun minCond(n: Int) : Int = downCond(n, 9, {a,b -> if (a < b) a else b}, {x -> x % 2 == 0 && x>3}) // минимальная четная цифра, больше 3, нач знач равно 9
 
 fun main() {
-    println("Введите число: ")
+    println("Введите число")
     val x = readLine()!!.toInt()
-    println("Сумма цифр: ${sum(x)}")
-    println("Произведение цифр: ${mult(x)}")
-    println("Максимальная цифра: ${max(x)}")
-    println("Минимальная  цифра: ${min(x)}")
+    println("Сумма нечетных цифр, больших 3: ${sumCond(x)}")
+    println("Произведение нечетных цифр, меньших 6: ${multCond(x)}")
+    println("Максимальная четная цифра, меньше 8: ${maxCond(x)}")
+    println("Минимальная четная цифра, больше 3: ${minCond(x)}")
 }
