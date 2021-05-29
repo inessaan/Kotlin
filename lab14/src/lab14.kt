6ada8583bd7c4347016bc5f18136a0a28723c389
Inessa An <inessa.an004@mail.ru>
2021-05-29 10:24:32 +0300

    задание 6 лаб14

--- lab14/src/lab14.kt
+++ lab14/src/lab14.kt
@@ -1,11 +1,11 @@
 tailrec fun downCond(n: Int, acum : Int, f : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
     if ( n == 0) acum else
         downCond(n/10, if (pr(n % 10)) f (n % 10, acum) else acum, f, pr)
-
+//рекурсией после спуска вниз,возвращаясь наверх, проверяет каждую цифру числа, если она удовлетворяет условию pr, то f работает над n
 
 fun sumCond(n: Int) : Int = downCond(n, 0, {a,b -> a + b}, {x -> x % 2 != 0 && x>3}) // сумма нечетных цифр, больших 3, нач счетчик равен 0
 fun multCond(n: Int) : Int = downCond(n, 1, {a,b -> a * b}, {x -> x % 2 != 0 && x<6}) // произведение нечетных цифр, меньших 6, нач счетчик равен 1
-fun maxCond(n: Int) : Int = downCond(n, 0, {a,b -> if (a > b) a else b}, {x -> x % 2 == 0 && x<8}) // максимальная четная цифру, меньше 8, нач знач равно 0
+fun maxCond(n: Int) : Int = downCond(n, 0, {a,b -> if (a > b) a else b}, {x -> x % 2 == 0 && x<8}) // максимальная четная цифра, меньше 8, нач знач равно 0
 fun minCond(n: Int) : Int = downCond(n, 9, {a,b -> if (a < b) a else b}, {x -> x % 2 == 0 && x>3}) // минимальная четная цифра, больше 3, нач знач равно 9
 
 fun main() {
