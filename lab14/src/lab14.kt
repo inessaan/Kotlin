56a10f6ac1450128407749cfe9c7a5d8a2fac650
Inessa An <inessa.an004@mail.ru>
2021-05-29 22:34:27 +0300

    Задание 7 лаб14

--- lab14/src/lab14.kt
+++ lab14/src/lab14.kt
@@ -1,18 +1,53 @@
-tailrec fun downCond(n: Int, acum : Int, f : (Int, Int) -> Int, pr : (Int) -> Boolean) : Int =
-    if ( n == 0) acum else
-        downCond(n/10, if (pr(n % 10)) f (n % 10, acum) else acum, f, pr)
-//рекурсией после спуска вниз,возвращаясь наверх, проверяет каждую цифру числа, если она удовлетворяет условию pr, то f работает над n
+fun sumDown(n : Int) : Int = sumDown(n, 0)
+tailrec fun sumDown(n : Int, sum : Int): Int = if(n == 0) sum else sumDown(n/10,sum+n%10)
 
-fun sumCond(n: Int) : Int = downCond(n, 0, {a,b -> a + b}, {x -> x % 2 != 0 && x>3}) // сумма нечетных цифр, больших 3, нач счетчик равен 0
-fun multCond(n: Int) : Int = downCond(n, 1, {a,b -> a * b}, {x -> x % 2 != 0 && x<6}) // произведение нечетных цифр, меньших 6, нач счетчик равен 1
-fun maxCond(n: Int) : Int = downCond(n, 0, {a,b -> if (a > b) a else b}, {x -> x % 2 == 0 && x<8}) // максимальная четная цифра, меньше 8, нач знач равно 0
-fun minCond(n: Int) : Int = downCond(n, 9, {a,b -> if (a < b) a else b}, {x -> x % 2 == 0 && x>3}) // минимальная четная цифра, больше 3, нач знач равно 9
+fun simple (x: Int) : Boolean = if (x == 1) false
+        else { if (x == 2) true
+        else simple (x, x/2)}
+
+tailrec fun simple(x:Int, i:Int): Boolean = if (i == 1) true
+    else { if(x % i == 0)
+        false
+    else  simple(x, i - 1)
+}
+
+fun sumSimpleDiv(x: Int) : Int = sumSimpleDiv(x, x/2, 0)
+tailrec fun sumSimpleDiv(x:Int, i: Int, accum: Int) :
+        Int = if (i == 1) accum + 1
+else {
+    if (x%i == 0 && simple(i))
+        sumSimpleDiv(x, i - 1, accum + i)
+    else sumSimpleDiv(x, i - 1, accum)
+}
+
+fun odd(x:Int): Int = odd (x, 0)
+tailrec fun odd(x: Int, count: Int) :
+        Int = if (x == 0) count else
+{
+    if ((x % 10) % 2 != 0 && (x % 10) > 3)
+        odd (x/10, count + 1)
+    else odd(x/10, count)
+}
+
+fun multDiv(x:Int): Int = multDiv(x,1,1)
+
+fun f(x:Int, div:Int): Boolean = x % div == 0
+
+tailrec fun multDiv(x:Int, div:Int, pr:Int): Int = if (x == div) pr else{
+    when {
+        sumDown(div) < sumDown(x) && f(x, div) ->
+        {
+            println("Делитель числа: $div" )
+            multDiv(x,div + 1,pr * div)
+        }
+        else -> multDiv(x,div + 1, pr)
+    }
+}
 
 fun main() {
-    println("Введите число")
+    println("Введите число: ")
     val x = readLine()!!.toInt()
-    println("Сумма нечетных цифр, больших 3: ${sumCond(x)}")
-    println("Произведение нечетных цифр, меньших 6: ${multCond(x)}")
-    println("Максимальная четная цифра, меньше 8: ${maxCond(x)}")
-    println("Минимальная четная цифра, больше 3: ${minCond(x)}")
+    println("Сума простых делителей числа = ${sumSimpleDiv(x)}")
+    println("Количество нечетных цифр числа, больших 3 = ${odd(x)}")
+    println("Произведение делителей числа, сумма цифр которых меньше, чем сумма цифр исходного числа = ${multDiv(x)}")
 }
